a6a4eedf167ae04251b8ffb8abee9068c5b0dbf7
Draneee <adrianaviladiaz93@gmail.com>
2025-11-20 00:57:12 -0500

    Refactor Home page for authentication handling
    - Replaced the existing Home component with an async function that checks user authentication status.
    - Implemented redirection to either the dashboard or login page based on authentication.
    - Simplified the Home component by removing the previous layout and content.

--- billing.app.cloud/app/login/page.tsx
+++ billing.app.cloud/app/login/page.tsx
@@ -21,11 +21,7 @@ export default function LoginPage() {
         </div>
       </div>
       <div className="bg-muted relative hidden lg:block">
-        <img
-          src="/placeholder.svg"
-          alt=""
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
+        <span className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale" />
       </div>
     </div>
   );
